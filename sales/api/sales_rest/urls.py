3a609087285fa31d37084fceee1bc56d3b8fe3b6
Anthony Nguyen <anguye18@fau.edu>
2024-02-06 12:22:22 -0500

    Sales view 1.0

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import api_list_customers,api_list_salesperson
+from .views import api_list_customers,api_list_salesperson,api_list_sale
 
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('customers/<int:id>/',api_list_customers,name="api_customer_detail"),
     path('salespeople/',api_list_salesperson,name="api_create_salespeople"),
     path('salespeople/<int:id>/',api_list_salesperson,name="api_salespeople_detail"),
+    path('sales/',api_list_sale,name="api_create_sale"),
+    path('sales/<int:id>/',api_list_sale,name="api_sale_detail"),
 ]
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Customer,Sale,Salesperson,AutomobileVO
 from common.json import ModelEncoder
 from django.views.decorators.http import require_http_methods
@@ -8,6 +8,7 @@ import json
 class SalespersonEncoder(ModelEncoder):
     model = Salesperson
     properties = [
+        "id",
         "first_name",
         "last_name",
         "employee_id"
@@ -16,11 +17,35 @@ class SalespersonEncoder(ModelEncoder):
 class CustomerEncoder(ModelEncoder):
     model = Customer
     properties = [
+        "id",
         "first_name",
         "last_name",
         "address",
         "phone_number"
     ]
+
+class AutomobileVOEncoder (ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "id",
+        "vin",
+        "sold"
+    ]
+
+class SaleEncoder(ModelEncoder):
+    model = Sale
+    properties = [
+        "id",
+        "automobile",
+        "salesperson",
+        "customer",
+        "price"
+    ]
+    encoders = {
+        "automobile": AutomobileVOEncoder(),
+        "salesperson": SalespersonEncoder(),
+        "customer": CustomerEncoder()
+    }
 
 @require_http_methods(["GET","POST","DELETE"])
 def api_list_customers(request,id=None):
@@ -29,7 +54,7 @@ def api_list_customers(request,id=None):
             customers = Customer.objects.all()
             return JsonResponse({"customers":customers},encoder=CustomerEncoder)
         else:
-            customers = Customer.objects.filter(id=id)
+            customers = get_object_or_404(Customer,id=id)
             return JsonResponse(customers,encoder=CustomerEncoder,safe=False)
     elif request.method == "POST":
         content = json.loads(request.body)
@@ -46,7 +71,7 @@ def api_list_salesperson(request,id=None):
             salespersons = Salesperson.objects.all()
             return JsonResponse({"salespeople":salespersons},encoder=SalespersonEncoder)
         else:
-            salespersons = Salesperson.objects.filter(id=id)
+            salespersons = get_object_or_404(Salesperson,id=id)
             return JsonResponse(salespersons,encoder=SalespersonEncoder,safe=False)
     elif request.method == "POST":
         content = json.loads(request.body)
@@ -55,3 +80,42 @@ def api_list_salesperson(request,id=None):
     else:
         count,_ = Salesperson.objects.filter(id=id).delete()
         return JsonResponse({"deleted":count>0})
+
+@require_http_methods(["GET","POST","DELETE"])
+def api_list_sale(request,id=None):
+    if request.method == "GET":
+        if id == None:
+            sales = Sale.objects.all()
+            return JsonResponse({"sales":sales},encoder=SaleEncoder)
+        else:
+            sales = get_object_or_404(Sale,id=id)
+            return JsonResponse(sales,encoder=SaleEncoder,safe=False)
+    elif request.method == "POST":
+        content = json.loads(request.body)
+        try:
+            automobile = AutomobileVO.objects.get(vin=content["automobile"])
+            salesperson = Salesperson.objects.get(id=content["salesperson"])
+            customer = Customer.objects.get(id=content["customer"])
+            content["automobile"] = automobile
+            content["salesperson"] = salesperson
+            content["customer"] = customer
+        except AutomobileVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid automobile id"},
+                status=400,
+            )
+        except Salesperson.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid salesperson id"},
+                status=400,
+            )
+        except Customer.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid customer id"},
+                status=400,
+            )
+        sale = Sale.objects.create(**content)
+        return JsonResponse(sale,encoder=SaleEncoder,safe=False)
+    else:
+        count,_ = Sale.objects.filter(id=id).delete()
+        return JsonResponse({"deleted:":count>0})
