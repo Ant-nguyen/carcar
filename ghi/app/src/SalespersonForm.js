06932edf057f2d0516a16748a4780a6afec5ca84
Anthony Nguyen <anguye18@fau.edu>
2024-02-06 15:10:24 -0500

    pre-pull

--- ghi/app/src/SalespersonForm.js
+++ ghi/app/src/SalespersonForm.js
@@ -25,7 +25,7 @@ function SalespersonForm(){
       let response = await fetch(url,fetchConfig)
       if(response.ok){
         let data = await response.json()
-        console.log("data")
+        console.log(data)
         setSalesForm({
           first_name:"",
           last_name:"",
@@ -42,12 +42,22 @@ function SalespersonForm(){
     <div className="my-5 container">
     <div className="offset-3 col-6">
         <div className="shadow p-4 mt-4">
-            <h1>Add a manufacturer</h1>
+            <h1>Add a Salesperson</h1>
             <form onSubmit={handleFormSubmit} id="create-manufacturer-form">
                 <div className="form-floating mb-3">
                     <input value={salesForm.first_name} onChange={hadleFormChange} placeholder="First name" required type="text" id="first_name" className="form-control"
                         name="first_name" />
-                    <label htmlFor="manufacturer">First Name</label>
+                    <label htmlFor="first_name">First Name</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input value={salesForm.last_name} onChange={hadleFormChange} placeholder="Last name" required type="text" id="last_name" className="form-control"
+                        name="last_name" />
+                    <label htmlFor="last_name">Last Name</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input value={salesForm.employee_id} onChange={hadleFormChange} placeholder="Employee ID" required type="text" id="employee_id" className="form-control"
+                        name="employee_id" />
+                    <label htmlFor="employee_id">Employee ID</label>
                 </div>
                 <button className="btn btn-primary">Create</button>
             </form>
